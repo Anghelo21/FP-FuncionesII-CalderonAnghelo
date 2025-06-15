e8eb06bbcb050c0abfe0b96e45abe2fbcfac49d7
AngheloCM <angheloprog@gmail.com>
2025-06-15 13:44:51 -0500

    Ejercicio 5 - Juego de dados por turnos, versión final

---
+++ Ejercicio7.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <cstdlib>
+#include <ctime>
+using namespace std;
+
+int main(){
+	srand(time(0));
+	int c1=0, c2=0;
+	cout<<"Iniciando juego..."<<endl;
+	do{
+		cout<<"Jugador 1 lanza un dado..."<<endl;
+		int j1 = (rand() % 6) + 1;
+		cout<<j1<<endl;
+		cout<<"Jugador 2 lanza un dado..."<<endl;
+		int j2 = (rand() % 6) + 1;
+		cout<<j2<<endl;
+		if (j1>j2){
+			cout<<"La ronda la gana el jugador 1 :D"<<endl;
+			c1=c1+1;
+		} else if(j2>j1){
+			cout<<"La ronda la gana el jugador 2 :D"<<endl;
+			c2=c2+1;
+		} else if(j1==j2){
+			cout<<"EMPATE"<<endl;
+		}
+	} while(c1 < 3 && c2 < 3);
+	cout<<"ENHORABUENA :D"<<endl;
+	if (c1==3){
+		cout<<"El juego lo gana el jugador 1"<<endl;
+	} else if(c2==3){
+		cout<<"El juego lo gana el jugador 2"<<endl;
+	}
+	return 0;
+}
